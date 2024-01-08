f9acd7db04ca4d1bab52cd55978c0b027d7fef3d
vdusin <vanessadusin@gmail.com>
2024-01-08 12:47:20 -0600

    ReadMe and File Structures

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # code-quiz
+
+## Description
+
+A simple Javascript multiple choice quiz that uses a timer that counds down from 175.
+
+## Installation
+
+N/A
+
+## Usage
+The user can answer the questions on the quiz while a timer counds down from 175. If they answer incorrectly, the timer will take off 15 seconds. The quiz starts after the user clicks on the "Start" button on the index page.
+
+Dev tools can be used by right clicking on the deployed wepage and clicking inspect. This will show the console to the right. 
+
+![screenshot1](assets/images/Screensht(23.png))
+
+
+## Credits
+
+I worked on this project with the help of a KU tutor.
+
+## License
+
+Please refer to the license in the repo.
--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,77 @@
+const button = document.querySelector(".btn")
+const questionTitle = document.querySelector(".questionTitle")
+const choicesContainer= document.querySelector(".choices")
+let currentQuestion = 0
+const TIMER =document.querySelector(".timer")
+
 // Questions
 var questions = [
     {
-        question 1: This is question 1!
-        Answer One
-        Answer Two 
-        Answer Three
+        question: "This is question 1!",
+        Choices: ["Answer 1", "Answer 2", "Answer 3"],
+        Answer: "Answer 1"
     },
     {
-        question 1: This is question 2!
-        Answer One
-        Answer Two 
-        Answer Three
+        question: "This is question 2!",
+        Choices: ["Answer 1a", "Answer 2a", "Answer 3a"],
+        Answer: "Answer 1a"
     },
     {
-        question 1: This is question 3!
-        Answer One
-        Answer Two 
-        Answer Three
-    }
+        question: "This is question 3!",
+        Choices: ["Answer 1b", "Answer 2b", "Answer 3b"],
+        Answer: "Answer 1b", 
+    },
+
 ]
 
 
 // TIMER
 // Selects element by class
-var secondsLeft = 10;
+var secondsLeft = 175;
 
 function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
     secondsLeft--;
 
+    TIMER.textContent=secondsLeft
+
     if(secondsLeft === 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
-      // Calls function to create and append image
-      sendMessage();
     }
 
   }, 1000);
 }
 
+function checkAnswer(){
+    console.log(this.dataset.value)
+    if (this.dataset.value !== questions[currentQuestion].answer) {
+        console.log("here")
+        secondsLeft=secondsLeft-15
+        TIMER.textContent=secondsLeft
+    }
+    else {
+        currentQuestion ++
+        startQuiz()
+    }
+}
 
 
+function startQuiz() {
+    questionTitle.textContent= questions[currentQuestion].question
+    choicesContainer.textContent=""
+    for (let index = 0; index < questions[currentQuestion].Choices.length; index++) {
+        var choiceBtn= document.createElement("button")
+        choiceBtn.textContent=questions[currentQuestion].Choices[index]
+        choiceBtn.setAttribute("data-value", questions[currentQuestion].Choices[index] )
+        choiceBtn.addEventListener("click", checkAnswer)
+        choicesContainer.appendChild(choiceBtn)
+    }
+}
 
 
-button.addEventListener("click", function());
+button.addEventListener("click", function(){
+    startQuiz()
+    setTime()
+});
